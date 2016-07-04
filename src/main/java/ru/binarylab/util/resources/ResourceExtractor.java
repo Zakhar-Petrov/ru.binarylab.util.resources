fd6211ddb13a2ce48db5bfd4b5680363b1a0e924
petrovz <pzakhar86@mail.ru>
2016-07-04 13:47:41 +0300

    Resource extractor for resource directory

---
+++ src/main/java/ru/binarylab/util/resources/ResourceExtractor.java
@@ -0,0 +1,44 @@
+package ru.binarylab.util.resources;
+
+import java.nio.file.Path;
+import java.util.Collection;
+
+public class ResourceExtractor {
+
+	private final ResourceEntry resourceEntry;
+
+	public ResourceExtractor(ResourceEntry resourceEntry) {
+		this.resourceEntry = resourceEntry;
+	}
+
+	public void extractToDirectory(Path destinationPath) throws ExtractException {
+		if (resourceEntry instanceof ResourceFile) {
+			extractFile((ResourceFile) resourceEntry, destinationPath);
+		} else if (resourceEntry instanceof ResourceDirectory) {
+			extractDirectory((ResourceDirectory) resourceEntry, destinationPath);
+		} else {
+			throw new IllegalStateException(
+					"Can't extract such type of resource: " + resourceEntry.getClass().getSimpleName());
+		}
+	}
+
+	private void extractFile(ResourceFile resourceFile, Path destinationPath) throws ExtractException {
+		new ResourceFileExtractor(resourceFile).extractToDirectory(destinationPath);
+	}
+
+	private void extractDirectory(ResourceDirectory resourceDirectory, Path destinationPath) throws ExtractException {
+		Collection<ResourceEntry> resourceEntries;
+		try {
+			resourceEntries = resourceDirectory.getChildren();
+		} catch (ResourceException e) {
+			throw new ExtractException("Can't get resurce directory structure", e);
+		}
+		for (ResourceEntry resourceEntry : resourceEntries) {
+			if (resourceEntry instanceof ResourceFile) {
+				ResourceFile resourceFile = (ResourceFile) resourceEntry;
+				extractFile(resourceFile, destinationPath);
+			}
+		}
+	}
+
+}
